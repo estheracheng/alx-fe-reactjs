f64bd41dbfb81aee75dc9e92ef0d30fc38c50458
estheracheng <estheracheng6@gmail.com>
2025-09-18 13:58:27 +0300

    fix tailwind error

---
+++ github-user-search/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust the path to match your source files
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];

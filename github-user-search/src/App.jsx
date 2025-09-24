439d8d857c2e316297c9f4a0a3bcce2ad710d462
estheracheng <estheracheng6@gmail.com>
2025-09-24 23:36:06 +0300

    tailwind directives

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,4 +1,5 @@
 import Search from './components/Search';
+import './index.css'
 
 function App() {
   return (
--- github-user-search/tailwind.config.js
+++ github-user-search/tailwind.config.js
@@ -1,12 +1,11 @@
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    // tailwind.config.js
+    export default {
+      content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    };

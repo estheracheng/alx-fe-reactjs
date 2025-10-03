d2c8f489177ca13753de6f19fbf07a222941f0d2
estheracheng <estheracheng6@gmail.com>
2025-10-03 11:01:00 +0300

    added recipe folder

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-export const content = [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-];
-export const theme = {
-    extend: {},
-};
+// /** @type {import('tailwindcss').Config} */
+// export const content = [
+//     "./index.html",
+//     "./src/**/*.{js,jsx,ts,tsx}",
+// ];
+// export const theme = {
+//     extend: {},
+// };
 export const plugins = [];
+ // eslint-disable-next-line no-undef
+ module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

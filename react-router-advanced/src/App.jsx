39bf9a0c4f99e981fa9e3d77b04b87f4a9acd4f0
estheracheng <estheracheng6@gmail.com>
2025-10-08 23:31:09 +0300

    added react router advanced

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -5,7 +5,7 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
 import Blog from './pages/Blog';
-import BlogPost from './pages/BlogPost';
+import BlogPost from './pages/BlogPost'; // Import dynamic post page
 import Profile from './components/Profile';
 import ProfileDetails from './pages/ProfileDetails';
 import ProfileSettings from './pages/ProfileSettings';
@@ -28,7 +28,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/blog" element={<Blog />} />
-              <Route path="/blog/:postId" element={<BlogPost />} />
+              <Route path="/blog/:postId" element={<BlogPost />} />  {/* Dynamic blog post route */}
               <Route path="/login" element={<Login />} />
               <Route path="/users" element={<Users />} />
               <Route path="/users/:userId" element={<UserProfile />} />
